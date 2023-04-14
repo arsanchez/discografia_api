7816194bbbd95ca508a640a34d8a01fd70bdd4ee
Argenis Sanchez <arsanchez@addi.com>
2023-04-14 10:41:06 -0400

    Added the requests needed to get all albums by artist

--- app/Http/Controllers/AlbumsController.php
+++ app/Http/Controllers/AlbumsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Response;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
  
 class AlbumsController extends Controller
 {
@@ -53,13 +54,71 @@ class AlbumsController extends Controller
         }
     }
 
-    public function getAllAlbums()
+    public function getAllAlbums(Request $request)
     {
-        if (!$this->client_id || !$this->client_secret) {
+        if (!$this->token_info) {
             return response()->json([
                 'errorMsg' => 'Invalid api credentials'
                 ],401); 
         }
-       dd($this->token_info);
+        
+        $request->validate([
+            'q' => 'required|max:255',
+        ]); 
+
+        // Lets search the artist be name
+        $name = $request->input('q');
+        $artist = $this->getArtistByName($name);
+
+        if (!$artist) {
+            return response()->json([
+                'errorMsg' => 'No artist found'
+                ],404); 
+        }
+
+        // Get artist discography
+        $albums = $this->getArtistDiscography($artist->id);
+        return response()->json($albums);
+    }
+
+    private function getArtistByName($name) 
+    {
+        $token = $this->token_info->access_token;
+        $search_endpoint = "https://api.spotify.com/v1/search?q=".$name."&type=artist";
+        $response = Http::withToken($token)->get($search_endpoint);
+
+        if ($response->successful())  {
+            $data  = json_decode($response->getBody());
+            // Getting the first match 
+            $artist  = $data->artists->items[0] ?? false;
+            return $artist;
+        } else {
+            return false;
+        }
+    }
+
+    private function getArtistDiscography($artist_id)
+    {
+        $token = $this->token_info->access_token;
+        $discographe_endpoint = "https://api.spotify.com/v1/artists/".$artist_id."/albums";
+        $response = Http::withToken($token)->get($discographe_endpoint);
+
+        if ($response->successful())  {
+            $data  = json_decode($response->getBody());
+            // Parsing the albums data
+            $albums = array_map(function($a) {
+                $parsed_data  = new \stdClass();
+                $parsed_data->name  = $a->name;
+                $parsed_data->released  = $a->release_date;
+                $parsed_data->tracks  = $a->total_tracks;
+                $parsed_data->cover  = $a->images[0];
+                
+                return $parsed_data;
+            }, $data->items);
+
+            return $albums;
+        } else {
+            return [];
+        }
     }
 }
